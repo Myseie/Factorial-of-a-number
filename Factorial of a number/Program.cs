1c7e657d8ded93ed0a32dea113ad353334876188
Myseie <123580372+Myseie@users.noreply.github.com>
2024-09-22 14:25:06 +0200

    Add files via upload

---
+++ Factorial of a number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main()
+    {
+        Console.Write("Ange en ett tal för att beräkna fakulteten :");
+        int n = int.Parse(Console.ReadLine());
+
+
+        long result = Faculty(n);
+
+        Console.WriteLine($"{n}! = {result}");
+
+
+    }
+
+    static long Faculty(int n)
+    {
+        if (n == 0 || n == 1)
+        {
+            return 1;
+        } else
+        {
+            return n * Faculty(n - 1);
+        }
+    }
+}
